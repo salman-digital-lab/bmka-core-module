331eab58380afc8087ea547d8de59f7ac5c0a7b3
fitrikarimah <fitri.karimah.98@gmail.com>
2024-03-19 22:05:07 +0700

    chore: add level index to dashboard

--- app/controllers/dashboard_controller.ts
+++ app/controllers/dashboard_controller.ts
@@ -4,7 +4,12 @@ import db from '@adonisjs/lucid/services/db'
 export default class DasbordAdminController {
   async CountProfiles({ response }: HttpContext) {
     try {
-      const profiles = await db.from('profiles').count('id as profile_amounts').groupBy('level')
+      const profiles = await db
+        .from('profiles')
+        .count('id as profile_amounts')
+        .groupBy('level')
+        .select('profiles.level')
+
       return response.ok({
         messages: 'GET_DATA_SUCCESS',
         data: profiles,
@@ -18,12 +23,22 @@ export default class DasbordAdminController {
   }
 
   async CountUsersGender({ response }: HttpContext) {
-    const countGender = await db.rawQuery(
-      `SELECT gender, COUNT(gender) AS jumlah FROM profiles GROUP BY gender`
-    )
-    return response.ok({
-      messages: 'GET_DATA_SUCCESS',
-      data: countGender[0],
-    })
+    try {
+      const profiles = await db
+        .from('profiles')
+        .count('id as profile_amounts')
+        .groupBy('gender')
+        .select('profiles.gender')
+
+      return response.ok({
+        messages: 'GET_DATA_SUCCESS',
+        data: profiles,
+      })
+    } catch (error) {
+      return response.internalServerError({
+        message: 'GENERAL_ERROR',
+        error: error.message,
+      })
+    }
   }
 }
